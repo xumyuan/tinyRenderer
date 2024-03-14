ff1d1027ac29af53109483fbca16dd86a659e94f
xumy <947656035@qq.com>
2024-03-14 16:40:21 +0800

    zbuffer

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ Model* model = NULL;
 const int width = 800;
 const int height = 800;
 
-
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
 	bool steep = false;
 	//对比直线的斜率，如果斜率大于1，就转置
@@ -55,41 +54,49 @@ void line(Vec2i v0, Vec2i v1, TGAImage& image, TGAColor color) {
 }
 
 // 求解重心坐标
-Vec3f barycentric(Vec2i* pts, Vec2i P) {
-	Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
-	/* `pts` and `P` has integer value as coordinates
-	   so `abs(u[2])` < 1 means `u[2]` is 0, that means
-	   triangle is degenerate, in this case return something with negative coordinates */
-	if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
-	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+Vec3f barycentric(Vec3f* pts, Vec3f P) {
+	auto A = pts[0];
+	auto B = pts[1];
+	auto C = pts[2];
+	Vec3f s[2];
+	for (int i = 2; i--; ) {
+		s[i][0] = C[i] - A[i];
+		s[i][1] = B[i] - A[i];
+		s[i][2] = A[i] - P[i];
+	}
+	Vec3f u = s[0] ^ s[1];
+	if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
-void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
-	// bouding box
-	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
-	Vec2i bboxmax(0, 0);
-	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+void triangle(Vec3f* pts, TGAImage& image, TGAColor color, float* zbuffer) {
+	Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+	Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
 	for (int i = 0; i < 3; i++) {
-		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
-
-		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
-		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+		for (int j = 0; j < 2; j++) {
+			bboxmin[j] = std::max(0.f, std::min(bboxmin[j], pts[i][j]));
+			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+		}
 	}
-	Vec2i P;
-	// 遍历bounding box
+
+	Vec3f P;
 	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
 		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-			// 求解重心坐标，根据正负判断是否在三角形中
 			Vec3f bc_screen = barycentric(pts, P);
 			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-			image.set(P.x, P.y, color);
+			P.z = 0;
+			for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+			if (zbuffer[int(P.x + P.y * width)] < P.z) {
+				zbuffer[int(P.x + P.y * width)] = P.z;
+				image.set(P.x, P.y, color);
+			}
 		}
 	}
 }
 
-void renderLineModel(Model* model) {
-	TGAImage image(width, height, TGAImage::RGB);
+void renderLineModel(Model* model, TGAImage& image) {
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
 		for (int j = 0; j < 3; j++) {
@@ -104,23 +111,37 @@ void renderLineModel(Model* model) {
 	}
 }
 
+Vec3f world2screen(Vec3f v) {
+	return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+}
+
 void renderTriangleModel(Model* model, TGAImage& image) {
 	Vec3f light_dir(0, 0, -1);
+	// zbuffer 初始化
+	float* zbuffer = new float[width * height];
+	for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
+
 	for (int i = 0; i < model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
-		Vec2i screen_coords[3];
+		Vec3f screen_coords[3];
 		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++) {
+			// 顶点坐标
 			world_coords[j] = model->vert(face[j]);
-			screen_coords[j] = Vec2i((world_coords[j].x + 1.) * width / 2.,
-				(world_coords[j].y + 1.) * height / 2.);
+			// 屏幕坐标
+			screen_coords[j] = world2screen(world_coords[j]);
 		}
-		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		Vec3f n = (world_coords[2] - world_coords[0]) ^
+			(world_coords[1] - world_coords[0]);
+
 		n.normalize();
+
 		float intensity = n * light_dir;
+
 		if (intensity > 0)
 			triangle(screen_coords, image,
-				TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+				TGAColor(intensity * 255, intensity * 255, intensity * 255, 255),
+				zbuffer);
 	}
 }
 
@@ -132,10 +153,10 @@ int main(int argc, char** argv) {
 		model = new Model("model/african_head.obj");
 	}
 
-	TGAImage image(800, 800, TGAImage::RGB);
+	TGAImage image(width, height, TGAImage::RGB);
 	renderTriangleModel(model, image);
 	image.flip_vertically(); // 将图像原点（0，0）放在左下角 
-	image.write_tga_file("resImg/head_triangle.tga");
+	image.write_tga_file("resImg/head_z.tga");
 
 
 	return 0;
