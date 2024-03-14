80ba4721f15a44dced0e39c286dc50d778eb8715
xumy <947656035@qq.com>
2024-03-14 14:38:24 +0800

    三角形光栅化

---
+++ src/main.cpp
@@ -0,0 +1,142 @@
+﻿#include <vector> 
+#include <iostream>
+#include "geometry.h"
+#include "tgaimage.h"
+#include "model.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
+
+
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+	bool steep = false;
+	//对比直线的斜率，如果斜率大于1，就转置
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	//保证起点在终点的左边
+	if (x0 > x1) {
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	//计算x变化1时，y的变化量
+	//如使用dy/dx，累加y的变化量超过0.5，y就加1
+	//使用2*dy，累加y的变化量超过dx，y就加1
+	//这样做的优势是没有乘除法，计算更快
+	int derror2 = std::abs(dy) * 2;
+	int error2 = 0;
+	int y = y0;
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+		error2 += derror2;
+		if (error2 > dx) {
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx * 2;
+		}
+	}
+}
+
+void line(Vec2i v0, Vec2i v1, TGAImage& image, TGAColor color) {
+	line(v0.x, v0.y, v1.x, v1.y, image, color);
+}
+
+// 求解重心坐标
+Vec3f barycentric(Vec2i* pts, Vec2i P) {
+	Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
+	/* `pts` and `P` has integer value as coordinates
+	   so `abs(u[2])` < 1 means `u[2]` is 0, that means
+	   triangle is degenerate, in this case return something with negative coordinates */
+	if (std::abs(u.z) < 1) return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color) {
+	// bouding box
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+	}
+	Vec2i P;
+	// 遍历bounding box
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			// 求解重心坐标，根据正负判断是否在三角形中
+			Vec3f bc_screen = barycentric(pts, P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+			image.set(P.x, P.y, color);
+		}
+	}
+}
+
+void renderLineModel(Model* model) {
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
+}
+
+void renderTriangleModel(Model* model, TGAImage& image) {
+	Vec3f light_dir(0, 0, -1);
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
+		for (int j = 0; j < 3; j++) {
+			world_coords[j] = model->vert(face[j]);
+			screen_coords[j] = Vec2i((world_coords[j].x + 1.) * width / 2.,
+				(world_coords[j].y + 1.) * height / 2.);
+		}
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0)
+			triangle(screen_coords, image,
+				TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+	}
+}
+
+int main(int argc, char** argv) {
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("model/african_head.obj");
+	}
+
+	TGAImage image(800, 800, TGAImage::RGB);
+	renderTriangleModel(model, image);
+	image.flip_vertically(); // 将图像原点（0，0）放在左下角 
+	image.write_tga_file("resImg/head_triangle.tga");
+
+
+	return 0;
+}
